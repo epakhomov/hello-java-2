769181447d861b533780e56e2809405842237444
Eugene Pakhomov <e.i.pakhomov@gmail.com>
2021-10-11 12:17:49 -0700

    Update HelloWorld.java

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        //String secret = "It's a secret to everybody.";
+        //try { javax.crypto.spec.SecretKeySpec keyspec = new javax.crypto.spec.SecretKeySpec(secret.getBytes("UTF-8"), "AES"); }
+        //catch (Exception e) { System.out.println("Something went wrong."); }
+        System.out.println("Hello World!");
+        
+ for (int i=0; i<10; i++) {
+            int j =i/i}
+        
+    }
+}
