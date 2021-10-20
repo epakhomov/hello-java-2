fea292a3eb0d8f633a9bac91376258d5af7bc288
Eugene Pakhomov <e.i.pakhomov@gmail.com>
2021-10-20 09:55:05 -0700

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -6,8 +6,7 @@ public class HelloWorld {
             System.out.println(i/i);
                 // afdsafsad
                 // sdfasfad
-            //asfda
-
+            //asfdadfg
   
     }
     }
